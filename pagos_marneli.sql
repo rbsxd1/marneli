36bb4db912f927e94834b7cfb626b88c65b484f1
rbsxd1 <rbsxd1@gmail.com>
2026-04-10 03:17:05 -0400

    Base de Datos Actualizada

--- pagos_marneli.sql
+++ pagos_marneli.sql
@@ -1,8 +1,72 @@
-CREATE TABLE pagos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    referencia VARCHAR(50) NOT NULL,
-    banco_emisor VARCHAR(100) NOT NULL,
-    fecha_pago DATE NOT NULL,
-    monto DECIMAL(10, 2), -- Por si quieres añadir el total
-    registro_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 10-04-2026 a las 09:13:27
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `pagos_marneli`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pagos`
+--
+
+CREATE TABLE `pagos` (
+  `id` int(11) NOT NULL,
+  `referencia` varchar(50) NOT NULL,
+  `banco_emisor` varchar(100) NOT NULL,
+  `fecha_pago` date NOT NULL,
+  `monto` decimal(10,2) DEFAULT NULL,
+  `emisor` varchar(100) DEFAULT NULL,
+  `registro_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `pagos`
+--
+
+INSERT INTO `pagos` (`id`, `referencia`, `banco_emisor`, `fecha_pago`, `monto`, `emisor`, `registro_at`) VALUES
+(1, '00123456', 'Banco de Venezuela', '2026-04-09', NULL, NULL, '2026-04-10 05:28:59'),
+(2, '010203040506', 'Banco de Venezuela', '2026-04-10', 100.00, 'Tekesito ', '2026-04-10 06:10:59');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `pagos`
+--
+ALTER TABLE `pagos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `referencia` (`referencia`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `pagos`
+--
+ALTER TABLE `pagos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
